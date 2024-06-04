f8987825bc5f3714c376e8bc3259ac496d921000
hit00-ling <167969614+hit00-ling@users.noreply.github.com>
2024-06-04 20:41:55 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,19 @@
-sloth.cali_by_button()
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+sloth.do_action(sloth.actions(sloth.action_name.walk), 5, 100)
+sloth.stand_still()
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
 basic.forever(function () {
 	
 })
